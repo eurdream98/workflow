2b0f4b67552287be383971942bb9bd6ba78680bd
eurdream98 <eurdream98@naver.com>
2024-03-19 12:48:04 +0900

    first-commit

---
+++ src/main/java/com/example/chap99workflow2/Chap99Workflow2Application.java
@@ -0,0 +1,13 @@
+package com.example.chap99workflow2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap99Workflow2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap99Workflow2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chap99workflow2/Chap99Workflow2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chap99workflow2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap99Workflow2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
